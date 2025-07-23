e7736980a807f385f8f1def52f969861874e17f8
Maria-Ramos01 <mfernanda0@hotmail.com>
2025-07-23 01:51:16 -0400

    definitiva_esta_si

--- manejo_notas/negocio/negocio_docentes.py
+++ manejo_notas/negocio/negocio_docentes.py
@@ -5,10 +5,10 @@ from auxiliares.mensajes import sin_datos
 def mostrar_listado_docentes():
     print()
     print('Listado de Docentes')
-    consulta = 'SELECT id, nombre_docente FROM docentes WHERE habilitado = 1'
+    consulta = 'SELECT id, nombre_docente, rut_docente, email_docente FROM docentes WHERE habilitado = 1'
     lista = leer_datos(consulta)
     tabla_docentes = PrettyTable()
-    tabla_docentes.field_names = ['Id', 'Nombre Docente']
+    tabla_docentes.field_names = ['Id', 'Nombre Docente', 'RUT Docente', 'Email Docente']
     if lista:
         for docente in lista:
             tabla_docentes.add_row(docente)
@@ -18,12 +18,14 @@ def mostrar_listado_docentes():
 
 def agregar_docente():
     nombre = input('Ingrese nombre del docente: ')
+    rut = input('Ingrese RUT del docente: ')
+    email = input('Ingrese email del docente: ')
     consulta = '''
-        INSERT INTO docentes (nombre_docente, habilitado)
-        VALUES (%s, 1)
+        INSERT INTO docentes (nombre_docente, rut_docente, email_docente, habilitado)
+        VALUES (%s, %s, %s, 1)
     '''
-    valores = (nombre.title(),)
-    if nombre:
+    valores = (nombre.title(), rut, email)
+    if nombre and rut and email:
         insertar_datos(consulta, valores)
         print('Docente agregado correctamente.')
     else:
@@ -33,13 +35,17 @@ def editar_docente():
     mostrar_listado_docentes()
     id_docente = input('Ingrese el ID del docente a editar: ')
     nuevo_nombre = input('Ingrese el nuevo nombre: ')
+    nuevo_rut = input('Ingrese el nuevo RUT: ')
+    nuevo_email = input('Ingrese el nuevo email: ')
     consulta = '''
         UPDATE docentes
-        SET nombre_docente = %s
+        SET nombre_docente = %s,
+            rut_docente = %s,
+            email_docente = %s
         WHERE id = %s
     '''
-    valores = (nuevo_nombre.title(), id_docente)
-    if id_docente and nuevo_nombre:
+    valores = (nuevo_nombre.title(), nuevo_rut, nuevo_email, id_docente)
+    if id_docente and nuevo_nombre and nuevo_rut and nuevo_email:
         insertar_datos(consulta, valores)
         print('Docente actualizado correctamente.')
     else:
